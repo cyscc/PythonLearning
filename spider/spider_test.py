6fe1822addd0eae8cd495197cff92b3471b472e6
cyss <2363067648@qq.com>
2022-05-18 16:56:59 +0800

    提取影片的超链接

--- spider/spider_test.py
+++ spider/spider_test.py
@@ -10,7 +10,20 @@ from urllib import *
 import sqlite3
 import xlwt
 
+# 影片链接
 findlink = re.compile(r'<a href="(.*?)">')
+# 影片图片
+findImgSrc = re.compile(r'<img.*src="(.*?)"', re.S)             # re.S: 忽视换行符
+# 影片片名
+findTitle = re.compile(r'<span class="title">(.*)</span>')
+# 影片评分
+findRating = re.compile(r'<span class="rating_num" property="v:average">(.*)</span>')
+# 评价人数
+findJudge = re.compile(r'<span>(\d*)人评价</span>')
+# 概况
+findInq = re.compile(r'<span class="inq">(.*)</span>')
+# 影片相关内容
+findBD = re.compile(r'<p class="">(.*?)</p>', re.S)
 
 
 # 得到一个指定网页的内容
@@ -35,25 +48,75 @@ def askUrl(url):
 
 def gatData(baseurl):
     datalist = []
-    for i in range(0, 1):
+    for i in range(0, 10):
         url = baseurl + str(i * 25)
         html = askUrl(url)
-
+        # 解析数据
+        data = []
         soup = BeautifulSoup(html, "html.parser")
         # print(soup)
         for item in soup.find_all("div", class_="item"):
-            print(item)
+            # print(item)
             item = str(item)
-            link = re.findall(findlink, item)[0]
-            # print(link)
 
+            link = re.findall(findlink, item)[0]
+            data.append(link)
+
+            imgSrc = re.findall(findImgSrc, item)[0]
+            data.append(imgSrc)
+
+            titles = re.findall(findTitle, item)
+            if len(titles) == 2:
+                ctitle = titles[0]
+                data.append(ctitle)
+                otitle = titles[1].replace("/", "")
+                data.append(otitle)
+            else:
+                data.append(titles[0])
+                data.append("  ")
+
+            rating = re.findall(findRating, item)[0]
+            data.append(rating)
+
+            judgeNum = re.findall(findJudge, item)[0]
+            data.append(judgeNum)
+
+            inq = re.findall(findInq, item)
+            if len(inq) == 0:
+                data.append("  ")
+            else:
+                data.append(inq[0].replace("。", ""))
+
+            bd = re.findall(findBD, item)[0]
+            bd = re.sub('<br(\s+)?/>(\s+)?', " ", bd)
+            bd = re.sub('/', " ", bd)
+            data.append(bd.strip())
+
+            datalist.append(data)
+    # print(datalist)
     return datalist
+
+
+def saveData(datalist, savepath):
+    print("save......")
+    book = xlwt.Workbook(encoding="utf-8", style_compression=0)
+    sheet = book.add_sheet("豆瓣电影250", cell_overwrite_ok=True)
+    col = ("电影详情链接", "图片链接", "影片中文名", "影片外文名", "评分", "评价数", "概况", "相关信息")
+    for i in range(0, 8):
+        sheet.write(0, i, col[i])
+    for i in range(0, 250):
+        print("第%d条" % (i + 1))
+        data = datalist[i]
+        for j in range(0, 8):
+            sheet.write(i+1, j, data[j])
+    book.save(savepath)
 
 
 def main():
     baseurl = "https://movie.douban.com/top250?start="
     datalist = gatData(baseurl)
-    savapath = ".\\doubanTop250.xls"
+    savapath = "doubanTop250.xls"
+    saveData(datalist, savapath)
 
 
 if __name__ == "__main__":
