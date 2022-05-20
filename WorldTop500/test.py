c4c58660c3ad507b94440987f7c41eeaa9fa91de
cyss <2363067648@qq.com>
2022-05-20 11:12:55 +0800

    世界500强

--- WorldTop500/test.py
+++
@@ -1,9 +0,0 @@
-# @createTime : 2022/5/20 9:22
-# @authorName : cyss
-# @fileName : test.py
-# @description :
-import re
-
-findNum = re.compile(r"[0-9]+")
-str1 = "erdsc3dsdc23423dv23e12"
-print(re.findall(findNum, str1))
